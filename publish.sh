af964c83befd56d44b75357bfb70f79a7d763397
夏治道(xiazhidao solio) <godsolio@hotmail.com>
2020-01-01 11:22:58 +0800

    publish.sh

---
+++ publish.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+. web.conf
+
+books=$(ls -l)
+webdoc=$WEB_DIR
+
+git reset --hard HEAD
+git clean -xdf
+
+for book in $books do
+    echo '$(date +"%Y-%m-%d %H:%M:%S") publish $book to $webdoc' &>> _pub.log
+end
