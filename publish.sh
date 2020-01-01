639ce64b62d26bac22a0fe43cce5efd2c5a2a5b2
夏治道(xiazhidao solio) <godsolio@hotmail.com>
2020-01-01 11:47:11 +0800

    publish.sh

--- publish.sh
+++ publish.sh
@@ -11,7 +11,7 @@ git clean -xdf
 for book in $books; 
 do
     echo $(date +"%Y-%m-%d %H:%M:%S") " publish $book to $webdoc" &>> ../_pub.log
-    if [ -f $book ] ;
+    if [ -d $book ] ;
     then
         cp -rf $book $webdoc
     fi
